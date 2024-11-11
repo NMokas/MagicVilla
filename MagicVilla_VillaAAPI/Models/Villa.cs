f01690d380daeb9319b59b180f7074f893c74c01
nmokas <bmtcarlos@gmail.com>
2024-11-11 15:44:58 +0000

    #DTO

---
+++ MagicVilla_VillaAAPI/Models/Villa.cs
@@ -0,0 +1,9 @@
+﻿namespace MagicVilla_VillaAAPI.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
